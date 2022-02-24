cfe4175d56d244a4cd68432dded9dc17d2f79a7b
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-02-24 14:17:31 +0000

    email generator

---
+++ mail/js/script.js
@@ -0,0 +1,32 @@
+let formAnswer = document.querySelector('.form-answer');
+
+
+let email = '@gmail.com';
+let emailList = ['luca', 'henry', 'gabriel', 'mauro', 'mattia','alessandro', 'anna'];
+
+
+
+let code = false
+
+let answer = prompt('inserisci un nome per generare un email')
+
+for (let i = 0; i < emailList.length; i++) {
+    if (emailList[i].toLowerCase() == answer.toLowerCase()) {
+        code = true
+    }
+}
+
+
+if (code == false) {
+    formAnswer.innerHTML = 'la tua email è: ' + answer.toLowerCase() + email;
+} else {
+    formAnswer.innerHTML ='questa email é gia utilizzata';
+}
+
+
+
+
+
+
+
+
